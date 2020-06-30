a65e9c8374bd5a9e726c968890f41ab6fcd26b51
a-yuto <guitarjoepass@gmail.com>
2020-06-30 17:43:20 +0900

    testを追加しました

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::collections::HashSet;
 
 fn newline(oneline: &str) -> String{
-    let reserved_word : HashSet<String>= vec!["SELECT".to_string(),"FROM".to_string()].into_iter().collect();
+    let reserved_word : HashSet<String>= vec!["SELECT".to_string(),"FROM".to_string(),"WHERE".to_string()].into_iter().collect();
     let mut now_word = "".to_string();
     let mut ans = "".to_string();
     for ch in oneline.chars() {
         if ch == ' '{
             if reserved_word.contains(&now_word){
-                ans = format!("{} \n{}",ans,now_word);    
+                ans = format!("{}\n{}",ans,now_word);    
             }else{
                 ans = format!("{} {}",ans,now_word);
             }
@@ -17,18 +17,28 @@ fn newline(oneline: &str) -> String{
         }
     }
     ans = format!("{} {}",ans,now_word);
-    ans[2..].to_string()
+    ans[1..].to_string()
 }
 
 fn main() {
-    println!("{}",newline("SELECT * FROM hoge"));
+    println!("{}",newline("SELECT * FROM hoge WHERE fuga"));
 }
 
 #[test]
-fn newline_test(){
+fn newline_test1(){
     let ans = 
-"SELECT * 
+"SELECT *
 FROM hoge".to_string();
     let test = "SELECT * FROM hoge";
+    assert_eq!(ans,newline(test));
+}
+
+#[test]
+fn newline_test2(){
+    let ans =
+"SELECT *
+FROM hoge
+WHERE fuga".to_string();
+    let test = "SELECT * FROM hoge WHERE fuga";
     assert_eq!(ans,newline(test));
 }
